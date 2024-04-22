c5f9610fdf08c6116e65902c4d3cacb3fcc0fc09
CamillaKleymen <camillakleymen@outlook.com>
2024-04-22 16:20:13 +0500

    weather

---
+++ main.py
@@ -0,0 +1,17 @@
+import requests
+
+token = '6223b9bb1c7800cfb61153c4405f954d'
+city = 'Tashkent'
+
+url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={token}'
+
+response = requests.get(url)
+
+if response.status_code == 200:
+
+    data = response.json()
+    weather_description = data['weather'][0]['description']
+    temperature = data['main']['temp']
+    print(f"Weather in {city}: {weather_description}, Temperature: {temperature}K")
+else:
+    print("Error in getting data:", response.status_code)
