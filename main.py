b1292fc0a32f268cb2c3ca106b8e68237d0d7f45
AxatSa <88314463+AxatSa@users.noreply.github.com>
2024-04-26 18:42:24 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import requests
+import regeyquest
 
 token = '6223b9bb1c7800cfb61153c4405f954d'
 city = 'Tashkent'
@@ -10,8 +10,8 @@ response = requests.get(url)
 if response.status_code == 200:
 
     data = response.json()
-    weather_description = data['weather'][0]['description']
-    temperature = data['main']['temp']
+     = data['weather'][0]['description']
+     = data['main']['temp']
     print(f"Weather in {city}: {weather_description}, Temperature: {temperature}K")
 else:
     print("Error in getting data:", response.status_code)
